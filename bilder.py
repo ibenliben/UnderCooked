992c2dfb25609b0d3af00265fa7e8612e1be0740
ibenliben <iben@reiremo.no>
2025-02-04 15:28:36 +0100

    la til gitignore

--- bilder.py
+++ bilder.py
@@ -8,8 +8,8 @@ player2_r = pg.image.load("assets/player2.png").convert_alpha()
 PLAYER_HEIGHT = 70
 
 bg_img = pg.transform.scale(bg_img, SIZE)
-player1_r = pg.transform.scale(player1_r, (PLAYER_HEIGHT, WIDTH/(HEIGHT/PLAYER_HEIGHT)))
-player2_r = pg.transform.scale(player2_r, (PLAYER_HEIGHT, WIDTH/(HEIGHT/PLAYER_HEIGHT)))
+player1_r = pg.transform.scale(player1_r, (PLAYER_HEIGHT, 1200/(975/PLAYER_HEIGHT)))
+player2_r = pg.transform.scale(player2_r, (PLAYER_HEIGHT, 1200/(975/PLAYER_HEIGHT)))
 
 
 player1_l = pg.transform.flip(player1_r, True, False)
