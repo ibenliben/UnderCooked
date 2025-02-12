cd006df9b898fa8cbc13a1805963d637ae52aa0c
ibenliben <iben@reiremo.no>
2025-02-12 20:00:15 +0100

    rydde - flytta ned food objects i object fil

--- object.py
+++ object.py
@@ -20,46 +20,6 @@ class Object:
         self.rect.y += self.dy
         #self.rect.topleft = (self.x, self.y)
 
-
-class Food(Object):
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
-
-
-class FoodStation(Object):
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
-
-    def give_food(self, player, Food_class, food_img):
-        if player.held_food is None:
-            player.pick_up(Food_class(player.rect.x, player.rect.y, food_img)) # spilleren får en tomat
-
-    # TODO: skille mellom give_tomato, give_salad osv.
-
-class Tomato(Food, pg.sprite.Sprite):
-    def __init__(self, x, y, image):
-        pg.sprite.Sprite.__init__(self)
-        super().__init__(x, y, image)
-        self.dx = 5
-        self.dy = -3
-        self.y_start = self.rect.y
-       
-    def update(self):
-        self.dy += 0.1
-
-        # stopper når den treffer bakken
-        if self.dy > 0 and abs(self.rect.y - self.y_start) < 0.1:
-            self.dx = 0
-            self.dy = 0
-            self.rect.y = self.y_start
-
-        super().update()
-
-        # TODO: 
-        # - må kunne plukkes opp etter kast
-        # - må kunne kutte opp tomat -> tomatoslice
-        # - må kunne steke kjøtt
-
 class Player(Object):
     def __init__(self, x, y, image):
         super().__init__(x, y, image) 
@@ -118,3 +78,43 @@ class Player(Object):
     def pick_up(self, food):
         if self.held_food is None:
             self.held_food = food
+
+
+class Food(Object):
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+
+
+class FoodStation(Object):
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+
+    def give_food(self, player, Food_class, food_img):
+        if player.held_food is None:
+            player.pick_up(Food_class(player.rect.x, player.rect.y, food_img)) # spilleren får en tomat
+
+    # TODO: skille mellom give_tomato, give_salad osv.
+
+class Tomato(Food, pg.sprite.Sprite):
+    def __init__(self, x, y, image):
+        pg.sprite.Sprite.__init__(self)
+        super().__init__(x, y, image)
+        self.dx = 5
+        self.dy = -3
+        self.y_start = self.rect.y
+       
+    def update(self):
+        self.dy += 0.1
+
+        # stopper når den treffer bakken
+        if self.dy > 0 and abs(self.rect.y - self.y_start) < 0.1:
+            self.dx = 0
+            self.dy = 0
+            self.rect.y = self.y_start
+
+        super().update()
+
+        # TODO: 
+        # - må kunne plukkes opp etter kast
+        # - må kunne kutte opp tomat -> tomatoslice
+        # - må kunne steke kjøtt
