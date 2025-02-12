7f57ffb5e859928d1f982f34089de56cb5ce4861
odahka <154480289+odahka@users.noreply.github.com>
2025-02-12 18:39:59 +0100

    Player Class lagt til
    Minste mann sletta hele player classen på tidligere commit. Nå er den lagt tilbake

--- object.py
+++ object.py
@@ -59,3 +59,57 @@ class Tomato(Food, pg.sprite.Sprite):
         # - må kunne kutte opp tomat -> tomatoslice
         # - må kunne steke kjøtt
 
+class Player(Object):
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image) 
+        self.held_food = None   # spilleren holder ingen mat ved start
+
+    def update(self, kd, ku, kr, kl, other_player):
+        self.dx = 3
+        self.dy = 3
+        keys_pressed = pg.key.get_pressed()
+        new_rect = self.rect.copy()
+
+        if keys_pressed[ku]:
+            new_rect.y += self.dy
+            if not self.check_collision(new_rect, other_player):
+                self.rect.y += self.dy
+
+        if keys_pressed[kd]:
+            new_rect.y -= self.dy
+            if not self.check_collision(new_rect, other_player):
+                self.rect.y -= self.dy
+
+        if keys_pressed[kr]:
+            new_rect.x += self.dx
+            if not self.check_collision(new_rect, other_player):
+                self.rect.x += self.dx
+
+        if keys_pressed[kl]:
+            new_rect.x -= self.dx
+            if not self.check_collision(new_rect, other_player):
+                self.rect.x -= self.dx
+                 
+        self.rect.topleft = (self.rect.x, self.rect.y)
+
+    def check_collision(self, new_rect, other_player):
+        if new_rect.colliderect(other_player.rect):
+            return True
+        #TODO: kollisjon med vegger
+        #for wall in walls:
+        #   if new_rect.colliderect(wall):
+        #       return True
+        return False
+
+    def throw(self, keys_pressed, k_throw, tomatoes):
+        if k_throw in keys_pressed and self.held_food:      # kan bare kaste mat hvis spilleren har mat
+            center = self.rect.center
+            tomato = Tomato(center[0]-10, center[1]-10, tomato_img)
+            tomatoes.add(tomato)
+            self.held_food = None       # spilleren mister maten
+
+        # TODO: all mat må kunne kastes - kan bare kaste maten spilleren holder. 
+    
+    def pick_up(self, food):
+        if self.held_food is None:
+            self.held_food = food
